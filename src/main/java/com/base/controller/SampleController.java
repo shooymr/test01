513f2f8af267b81e5f7191137cd20b19bfcbe5ee
son <shshshoo@naver.com>
2021-01-31 18:28:47 +0900

    처음 커밋

---
+++ src/main/java/com/base/controller/SampleController.java
@@ -0,0 +1,16 @@
+package com.base.controller;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+    @RequestMapping(value="/")
+    @ResponseBody
+    public String sampleHome() {
+        return "Hello Gradle! Hello Spring Boot!";
+    }
+}
